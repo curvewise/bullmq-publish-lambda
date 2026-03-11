ab6d35863dbd2417dff95555d5ed934c9421eb16
jbwhite <josephbarrywhite@gmail.com>
2026-03-11 15:01:54 -0400

    move options to Queue setup; remove options set by worker

--- src/handler.ts
+++ src/handler.ts
@@ -18,6 +18,13 @@ function getQueue(): Queue {
   if (!queue) {
     queue = new Queue(queueName, {
       connection: { url: redisUrl },
+      defaultJobOptions: {
+        attempts: 2,
+        backoff: {
+          type: 'exponential',
+          delay: 1000 * 120, // 2 minutes
+        },
+      },
     })
   }
   return queue
@@ -31,15 +38,7 @@ export async function handler(event: Input, context: any): Promise<void> {
   const { taskIdentifier, payload } = event
 
   console.log(`Publishing to queue with taskIdentifier ${taskIdentifier}`)
-  await queue.add(taskIdentifier, payload, {
-    attempts: 2,
-    removeOnFail: 50,
-    removeOnComplete: true,
-    backoff: {
-      type: 'exponential',
-      delay: 1000 * 120, // 2 minutes
-    },
-  })
+  await queue.add(taskIdentifier, payload)
   console.log(
     `Finished publishing to queue with taskIdentifier ${taskIdentifier}`,
   )
